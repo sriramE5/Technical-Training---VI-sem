5323e4b9ee32738fedf871952f729dfd7e9d0bde
Chodabattula Sai Maneesh Sri Ram <153256696+sriramE5@users.noreply.github.com>
2026-03-06 13:11:17 +0530

    Update chefRichProblem.java

--- Week-8/chefRichProblem.java
+++ Week-8/chefRichProblem.java
@@ -1,7 +1,21 @@
-# cook your dish here
-t = int(input())
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-for _ in range(t):
-    A, B, X = map(int, input().split())
-    years = (B - A) // X
-    print(years)
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-- > 0){
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    int X = sc.nextInt();
+		    
+		    int years = (B - A) / X;
+		    System.out.println(years);
+		}
+	}
+}
